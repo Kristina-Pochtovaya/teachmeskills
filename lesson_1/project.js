a7b29b036817924ada9fe32ddf231e59b417a728
Kristina <pochtovaya.kristina@mail.ru>
2025-10-20 21:40:19 +0300

    add small fixes

--- lesson_1/project.js
+++ lesson_1/project.js
@@ -1,72 +1,71 @@
-const fs = require('fs')
-const http = require('http')
-const EventEmmiter = require('events')
-const { Transform, pipeline }  = require('stream')
+const fs = require("fs");
+const http = require("http");
+const EventEmmiter = require("events");
+const { Transform, pipeline } = require("stream");
 
-const PORT = 3000
+const PORT = 3000;
 
 class Logger extends EventEmmiter {
-    log(message) {
-        this.emit('log', `✅ ${message}`)
-    }
-    warn(message) {
-     this.emit('log', `⚠️ ${message}`)
-    }
-    error(message) {
-       this.emit('log', `❌ ${message}`)
-    }
+  log(message) {
+    this.emit("log", `✅ ${message}`);
+  }
+  warn(message) {
+    this.emit("log", `⚠️ ${message}`);
+  }
+  error(message) {
+    this.emit("log", `❌ ${message}`);
+  }
 }
 
 class UpperCaseStream extends Transform {
- _transform(chunk, encoding, callback) {
-    callback(null, chunk.toString().toUpperCase())
- }
+  _transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().toUpperCase());
+  }
 }
 
-const logger = new Logger()
+const logger = new Logger();
 
-logger.on('log', (msg) => console.log(msg))
-logger.on('warn', (msg) => console.log(msg))
-logger.on('error', (msg) => console.log(msg))
+logger.on("log", (msg) => console.log(msg));
+logger.on("warn", (msg) => console.warn(msg));
+logger.on("error", (msg) => console.error(msg));
 
 const server = http.createServer((req, res) => {
-    if(req.method === 'GET' && req.url === '/') {
-        const src = fs.createReadStream('data.txt')
-        const upper = new UpperCaseStream()
+  if (req.method === "GET" && req.url === "/") {
+    const src = fs.createReadStream("data.txt");
+    const upper = new UpperCaseStream();
 
-        let bytesRead = 0
+    let bytesRead = 0;
 
-        src.on('data', (chunk) => bytesRead+=chunk.length)
+    src.on("data", (chunk) => (bytesRead += chunk.length));
 
-        src.on('end', () => {
-            if(bytesRead === 0) {
-             if (!res.writableEnded) {
-                    res.writeHead(500, {'content-type': 'text/plain; charset=utf-8'});
-                    res.end( logger.warn('Файл пуст!'));
-                }
-            }
-        })
+    src.on("end", () => {
+      if (bytesRead === 0) {
+        if (!res.writableEnded) {
+          res.writeHead(500, { "content-type": "text/plain; charset=utf-8" });
+          res.end(logger.warn("Файл пуст!"));
+        }
+      }
+    });
 
-        const dest = fs.createWriteStream('upperCasedata.txt');
+    const dest = fs.createWriteStream("upperCasedata.txt");
 
-        pipeline(src, upper , dest, (err) => {
-            if(err) {
-                logger.error(err.message)
+    pipeline(src, upper, dest, (err) => {
+      if (err) {
+        logger.error(err.message);
 
-                if(!res.headersSent) {
-                      res.writeHead(500, {'content-type': 'text/plain, charset=utf-8'});
-                }
+        if (!res.headersSent) {
+          res.writeHead(500, { "content-type": "text/plain, charset=utf-8" });
+        }
 
-                if(!res.writableEnded) {
-                    res.end(logger.error('Ошибка обработки файла!'))
-                }
-            }
-        })
+        if (!res.writableEnded) {
+          res.end(logger.error("Ошибка обработки файла!"));
+        }
+      }
+    });
+  } else {
+    res.writeHead(404, { "content-type": "text/plain; charset=utf-8" });
+    res.end(logger.error("NOT FOUND!"));
+  }
+});
 
-    } else {
-         res.writeHead(404, {'content-type': 'text/plain; charset=utf-8'});
-          res.end(logger.error('NOT FOUND!'));
-    }
-})
-
-server.listen(PORT, () => logger.log('HTTP сервер запущен!'))
+server.listen(PORT, () => logger.log("HTTP сервер запущен!"));
