3102880c3389a5f45746afd7a58682620d668279
Kristina <pochtovaya.kristina@mail.ru>
2025-12-02 20:45:51 +0200

    add lesson 13

---
+++ microservices_project/gateway/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'USER_SERVICE',
+      useFactory: () =>
+        ClientProxyFactory.create({
+          // transport: Transport.TCP,
+          // options: { host: '127.0.0.1', port: 3001 },
+          transport: Transport.REDIS,
+          options: { host: '127.0.0.1', port: 6379 },
+        }),
+    },
+  ],
+})
+export class AppModule {}
